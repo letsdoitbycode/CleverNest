ffcdf3044bf426dc91b6cd183d8e5eab1f2be819
letsdoitbycode <bhosale.akshay1305@gmail.com>
2024-10-07 21:16:59 +0530

    Here it letsdoitbycode

--- chatBOT/script.js
+++ chatBOT/script.js
@@ -5,7 +5,7 @@ const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
 
 let userMessage = null; // Variable to store user's message
-const API_KEY = "sk-fl8dIFUt633_T5bVF2R1IPhpEce0X0GwhbR43UN7tlT3BlbkFJSD2TKrQsxX0A4QWzGXVJXa3pl0OhwRq-eERjnNtZoA"; // Paste your API key here
+const API_KEY = "OPENAI KEY"; // Paste your API key here
 const inputInitHeight = chatInput.scrollHeight;
 
 const createChatLi = (message, className) => {
